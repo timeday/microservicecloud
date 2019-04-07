921b0423a24cc620033694f9ecdf6a117b901915
zhw <hongwei.zhang@maobc.com>
2019-04-07 23:09:05 +0800

    Initial commit

---
+++ microservicecloud-provider-dept-8003/src/main/java/com/demo/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.demo.springcloud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.springcloud.dao.DeptDao;
+import com.demo.springcloud.entities.Dept;
+import com.demo.springcloud.service.DeptService;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
